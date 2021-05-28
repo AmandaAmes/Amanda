a26f9d32c1956e1653ffbfe086049545311b51b5
Amanda Ames <40763714+AmandaAmes@users.noreply.github.com>
2021-05-28 11:00:40 -0700

    init

---
+++ CSharp/DriverLicense.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace CSharp
+{
+    public static class DriverLicense
+    {
+        /// <summary>
+        /// Consider this list of formats: https://ntsi.com/drivers-license-format/
+        /// Validate Driver's license number, implement Nebraska and Mississippi in an expandable way to eventually handle all US states.
+        /// Fail validation if unexpected data is passed in.
+        /// Nebraska: 1Alpha+6-8Numeric
+        /// Mississippi: 9Numeric
+        /// </summary>
+        /// <param name="licenseNumber"></param>
+        /// <param name="stateCode"></param>
+        /// <returns></returns>
+        public static bool Validate(string licenseNumber, string stateCode)
+        {
+            string strLicenseFormat = "";
+
+            if (stateCode == "NE") strLicenseFormat = "1Alpha+6-8Numeric";
+            else if (stateCode == "MS") strLicenseFormat = "9Numeric";
+            else if (stateCode == "CO") strLicenseFormat = "9Numeric or 1Alpha+3-6Numeric or 2Alpha+2-5Numeric";
+            else
+            {
+                // other states not implemented yet
+                return false;
+            }
+
+            //Parse the format into an array as some states have multiple formats (separated by " or ")
+            strLicenseFormat = strLicenseFormat.Replace(" or ", "|");
+            strLicenseFormat = strLicenseFormat.Replace(" ", ""); //remove spaces (if any)
+            string[] strFormatArray = strLicenseFormat.Split("|".ToCharArray());
+            string strRegexPattern;
+            foreach (string strFormat in strFormatArray)
+            {
+                string[] strSegmentArray = strFormat.Split("+".ToCharArray());
+                strRegexPattern = "";
+                foreach (string strSegment in strSegmentArray)
+                {
+                    if (strSegment.Contains("Alpha")) strRegexPattern += "[A-Za-z]{" + strSegment.Replace("Alpha", "").Replace("-",",") + "}";
+                    else if (strSegment.Contains("Numeric")) strRegexPattern += "[0-9]{" + strSegment.Replace("Numeric", "").Replace("-", ",") + "}";
+                    else { return false; } //invalid format string 
+
+                }
+                strRegexPattern = "^" + strRegexPattern + "$"; //check the entire string
+                if (Regex.IsMatch(licenseNumber, strRegexPattern)) return true;
+            }
+
+            // at this point, the string failed to match a pattern
+            return false;
+        }
+    }
+}
---
+++ CSharp/Strings.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public static class Strings
+    {
+        /// <summary>
+        /// Implement a string reversal method
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Reverse1(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        /// <summary>
+        /// Implement a different string reversal method
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Reverse2(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            StringBuilder builder = new StringBuilder(input.Length);
+            for (int i = (input.Length-1); i>=0; i--)
+            {
+                builder.Append(input[i]);
+            }
+
+            return builder.ToString();
+        }
+
+
+        /// <summary>
+        /// Implement a string truncation function that safely truncates the input without throwning an exception. Return null if input is null.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string SafeTruncate(string input, int length)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            if (length <= 0) return "";
+
+            if (input.Length <= length)
+                return input;
+            else
+                return input.Substring(0, length);
+
+        }
+
+        /// <summary>
+        /// return a list of even numbers from the input.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static List<int> EvenNumerics(List<string> input)
+        {
+            List<int> strResultList = new List<int>();
+            int i;
+            foreach(string strItem in input)
+            {
+                if (int.TryParse(strItem, out i))
+                {
+                    if (i % 2 == 0) strResultList.Add(i);
+                }
+            }
+            return strResultList;
+        }
+    }
+}
